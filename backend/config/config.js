d429aab44111c679912cf72bf7398b9d37590e47
EddieVallejos <eddievallejos08@gmail.com>
2017-03-09 06:42:20 +0800

    Updated the README.md

---
+++ backend/config/config.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const path  = require('path');
+
+const config = {
+    APP_NAME: 'MalICSi App: A PalICSihan Sport Scheduling App',
+    APP_URL: 'http://localhost:8000',
+
+    PORT: 8000,
+    STATIC_PORT: 8080,
+    IP: '127.0.0.1',
+
+    DEVELOPMENT: {
+        host: 'localhost',
+        user: 'root',
+        password: 'PolaTan1012',
+        database: 'malicsi_app'
+    },
+
+    ASSETS_DIR: path.normalize(__dirname + '/../front-end'),
+
+    COOKIE_SECRET: 'm4L1cSi_a5L',
+    COOKIE_NAME: '__t0p_S3cR3T',
+    COOKIE_DOMAIN: '.malICSi.io'
+
+};
+
+module.exports = config;
---
+++ backend/server.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const config        = require(__dirname + '/config/config');
+const express       = require('express');
+const body_parser   = require('body-parser');
+const redis         = require('redis');
+const redis_store    = require('connect-redis')(session);
+const client        = redis.createClient();
+
+let handler = start();
+let app;
+
+let start = () => {
+    
+    if (handler) {
+        handler.close();
+    }
+
+    // create an express app
+    app = express();
+
+    // setting the environment for express
+    app.set('case sensitive routing', true);
+    app.set('x-powered-by', false);
+    // incorporating the session to the app for usage
+    app.use(session({
+        secret: config.COOKIE_SECRET,
+        resave: false,
+        saveUninitialized: true, 
+        cookie: {maxAge: 60 * 1000 * 60 * 2 }, 
+        store: new redis_store({
+            host: 'localhost',
+            port: config.PORT,
+            client: client
+        })
+    }))
+    // other packages that is needed to make the app secured and stable
+    app.use(express.static(__dirname + '/../front-end'));
+    app.use(require('method-override')());
+    app.use(body_parser.urlencoded({extended: true}));
+    app.use(body_parser.json());
+    app.use(require('compression')());
+    // this will start app
+    return app.listen(config.PORT, config.IP);
+
+}
+
+module.exports = {
+    app,
+    start,
+    handler
+}
