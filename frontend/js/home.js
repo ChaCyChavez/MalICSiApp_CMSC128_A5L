d5d1a52a0b0fc5603fa440bf5d0934cac77518c0
edwardPascual <jppascual2@up.edu.ph>
2017-03-10 16:12:21 +0800

    Added modal of login and register

---
+++ frontend/js/home.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  
+  $('.modal').modal({
+    // size: 'small',
+    dismissible: true,
+    opacity: 0.5,
+    in_duration: 300,
+    out_duration: 200,
+    starting_top: '4%',
+    ending_top: '10%',
+  });
+
+  $(".modal").width($(".modal").width());
+  $(".modal").height($(".modal").height());
+
+});
