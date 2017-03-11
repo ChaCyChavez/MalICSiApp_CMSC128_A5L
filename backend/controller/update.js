182f0d14ceb83d0618f43f824769440035f3d5c5
Ken Mercado <mightymercado@gmail.com>
2017-03-12 05:42:36 +0800

    added update controller

---
+++ backend/controller/update.js
@@ -0,0 +1,96 @@
+'use strict';
+
+const db = require(__dirname + '/../lib/mariasql');
+
+exports.update_account = (res, req, next) => {
+	const query_string = 'UPDATE account set email = ?, username = ?, password = ?, firstname = ?, middlename = ?, lastname = ?, course = ?, birthday = ?, college = ?, status = ? WHERE account id = ?;';
+	const payload = [req.body.email, req.body.username, req.body.password, req.body.firstname, req.body.middlename, req.body.lastname, req.body.course, req.body.birthday, req.body.college, req.body.status, req.body.account_id];
+    const callback = (err, data) => {
+		req.send(data);
+	};
+
+	db.query(query_string, payload, callback);
+};
+
+exports.update_game = (res, req, next) => {
+	const query_string = 'UPDATE game set game_name = ?, game_place = ?, game_starting_time_date = ?, game_ending_time_date = ? WHERE game_id = ?;';
+	const payload = [req.body.game_name, req.body.game_place, req.body.game_starting_time_date, req.body.game_ending_time_date, req.body.account_id];
+    const callback = (err, data) => {
+		req.send(data);
+	};
+
+	db.query(query_string, payload, callback);
+};
+
+
+exports.update_team = (res, req, next) => {
+	const query_string = 'UPDATE team set team_name = ?, team_color = ?, team_coach = ? WHERE team_id = ?;';
+	const payload = [req.body.team_name, req.body.team_color, req.body.team_coach, req.body.team_id];
+    const callback = (err, data) => {
+		req.send(data);
+	};
+
+	db.query(query_string, payload, callback);
+};
+
+
+
+exports.update_player = (res, req, next) => {
+	const query_string = 'UPDATE player set player_jersey_number = ?, is_coach = ?, game_id = ? WHERE player_id = ?;';
+	const payload = [req.body.player_jersey_number, req.body.is_coach, req.body.game_id, req.body.player_id];
+    const callback = (err, data) => {
+		req.send(data);
+	};
+
+	db.query(query_string, payload, callback);
+};
+
+exports.update_court = (res, req, next) => {
+	const query_string = 'UPDATE court set court_name = ?, court_location = ?, court_type = ? WHERE court_id = ?;';
+	const payload = [req.body.court_name, req.body.court_location, req.body.court_type, req.body.court_id];
+    const callback = (err, data) => {
+		req.send(data);
+	};
+
+	db.query(query_string, payload, callback);
+};
+
+exports.update_sport = (res, req, next) => {
+	const query_string = 'UPDATE sport set sport_type_name = ?, number_of_participants = ?, date_time = ?, division = ?, court_id = ? WHERE sport_id = ?;';
+	const payload = [req.body.sport_type_name, req.body_number_of_participants, req.body.date_time, req.body.division, req.body.court_id, req.body.sport_id];
+    const callback = (err, data) => {
+		req.send(data);
+	};
+
+	db.query(query_string, payload, callback);
+};
+
+exports.update_sport = (res, req, next) => {
+	const query_string = 'UPDATE sponsor set sponsor_name = ?, sponsor_affiliation = ?, sponsor_logo = ? WHERE sponsor_id = ?;';
+	const payload = [req.body.sponsor_name, req.body.sponsor_affiliation, req.body.sponsor_logo, req.body.sponsor_id];
+    const callback = (err, data) => {
+		req.send(data);
+	};
+
+	db.query(query_string, payload, callback);
+};
+
+exports.update_score = (res, req, next) => {
+	const query_string = 'UPDATE score set series = ?, winning_team = ?, losing_team = ? WHERE score_id = ?;';
+	const payload = [req.body.series, req.body.winning_team, req.body.losing_team, req.body.score_id];
+    const callback = (err, data) => {
+		req.send(data);
+	};
+
+	db.query(query_string, payload, callback);
+};
+
+exports.update_game_game_type = (res, req, next) => {
+	const query_string = 'UPDATE game_game_typeset game_type = ? WHERE game_id = ? AND game_type = ?;';
+	const payload = [req.body.game_type, req.body.game_id, req.body.game_type];
+    const callback = (err, data) => {
+		req.send(data);
+	};
+
+	db.query(query_string, payload, callback);
+};
