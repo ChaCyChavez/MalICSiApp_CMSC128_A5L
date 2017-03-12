fd4c9890c8aca4d5f716b5e30b63115bb48d7b17
Ken Mercado <mightymercado@gmail.com>
2017-03-12 12:29:55 +0800

    fixed controller typo

--- backend/router.js
+++ backend/router.js
@@ -4,15 +4,15 @@ const controller = require(__dirname + '/controller/update');
 var path = require('path');
 
 module.exports = (router) => {
-    router.post('/api/update-account', controller.update_account);
-    router.post('/api/update-game', controller.update_game);
-    router.post('/api/update-team', controller.update_team);
-    router.post('/api/update-player', controller.update_player);
-    router.post('/api/update-court', controller.update_court);
-    router.post('/api/update-sport', controller.update_sport);
-    router.post('/api/update-sponsor', controller.update_sponsor);
-    router.post('/api/update-score', controller.update_score);
-    router.post('/api/update-game-type', controller.update_game_type);
+    router.post('/api/update-account', update.update_account);
+    router.post('/api/update-game', update.update_game);
+    router.post('/api/update-team', update.update_team);
+    router.post('/api/update-player', update.update_player);
+    router.post('/api/update-court', update.update_court);
+    router.post('/api/update-sport', update.update_sport);
+    router.post('/api/update-sponsor', update.update_sponsor);
+    router.post('/api/update-score', update.update_score);
+    router.post('/api/update-game-type', update.update_game_type);
 
     router.all('*', (req, res, next) => {
         res.status(404).send({
