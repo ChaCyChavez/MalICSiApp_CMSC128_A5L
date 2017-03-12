59346d441e50fec04805c414c76af5bd193bd910
chacychavez <cschavez@up.edu.ph>
2017-03-12 13:54:16 +0800

    Changed some colors

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,22 +1,15 @@
 $(document).ready(function(){
   $('.carousel.carousel-slider').carousel({fullWidth: true});
-  $('#butones').sideNav(
-        {
-            menuWidth: 300, // Default is 300
-            edge: 'left', // Choose the horizontal origin
-            closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-            draggable: true // Choose whether you can drag to open on touch screens
-        }
-    );
-     $('#dr').dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      constrainWidth: false, // Does not change width of dropdown to that of the activator
-      hover: false, // Activate on hover
-      gutter: 0, // Spacing from edge
-      belowOrigin: true, // Displays dropdown below the button
-      alignment: 'left', // Displays dropdown with edge aligned to the left of button
-      stopPropagation: false // Stops event propagation
-    }
-  );
+  $('.modal').modal({
+    // size: 'small',
+    dismissible: true,
+    opacity: 0.5,
+    in_duration: 300,
+    out_duration: 200,
+    starting_top: '4%',
+    ending_top: '10%',
+  });
+
+  // $(".modal").width($(".modal").width());
+  // $(".modal").height($(".modal").height());
 });
