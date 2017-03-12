1a4a739803fe78a54d968e8068f3ab204afe5633
CarlJanoras <janorascarlxd@gmail.com>
2017-03-12 18:52:54 +0800

    updated setup.sh

--- databaes/setup.sh
+++ databaes/setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+echo "[root password]"			 &&
 mysql -uroot -p < tables.sql     && echo "Created Tables"                  && 
+echo "[root password]"			 &&
 mysql -uroot -p < data.sql       && echo "Inserted Dummy Data"
