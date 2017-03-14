49e9006773b27b6689d2c0f3dc2a5daba268ed79
CarlJanoras <janorascarlxd@gmail.com>
2017-03-14 13:25:21 +0800

    updated using backend convention

--- backend/controller/retrieve.js
+++ backend/controller/retrieve.js
@@ -10,7 +10,7 @@ exports.get_account = (req, res, next) => {
       +"account_id = ?;";  
   const payload = [req.params.account_id];
   const callback = (err, data) => {
-     res.send(data);
+    res.send(data);
  };
 
   db.query(query_string, payload, callback);
@@ -21,7 +21,7 @@ exports.get_all_game = (req, res, next) => {
   const query_string = "SELECT * from game, team;"; 
   const payload = [];
   const callback = (err, data) => {
-     res.send(data);
+    res.send(data);
  };
 
   db.query(query_string, payload, callback);
@@ -30,10 +30,11 @@ exports.get_all_game = (req, res, next) => {
 
 //View All of the Current/Live Games
 exports.get_current_game = (req, res, next) => {
-  const query_string = "SELECT * from game where datediff(now(), game_ending_time_date) > 0;";  
+  const query_string = "SELECT * from game where datediff(now(), "
+      +"game_ending_time_date) > 0;";  
   const payload = [];
   const callback = (err, data) => {
-     res.send(data);
+    res.send(data);
   };
 
   db.query(query_string, payload, callback);
@@ -44,7 +45,7 @@ exports.get_player = (req, res, next) => {
   const query_string = "SELECT * from player, account where player_id = ?;";  
   const payload = [req.params.player_id];
   const callback = (err, data) => {
-     res.send(data);
+    res.send(data);
   };
 
   db.query(query_string, payload, callback);
@@ -55,7 +56,7 @@ exports.get_winner = (req, res, next) => {
   const query_string = "SELECT * from game, score where game_id = ?;";  
   const payload = [req.params.player_id];
   const callback = (err, data) => {
-     res.send(data);
+    res.send(data);
   };
 
   db.query(query_string, payload, callback);
@@ -63,10 +64,11 @@ exports.get_winner = (req, res, next) => {
 
 //View Sponsoring institution for a game
 exports.get_sponsor = (req, res, next) => {
-  const query_string = "SELECT * from sponsor where sponsor_id in (SELECT sponsor_id from game_sponsor where game_id = ?);";  
+  const query_string = "SELECT * from sponsor where sponsor_id in (SELECT " 
+      +"sponsor_id from game_sponsor where game_id = ?);";  
   const payload = [req.params.game_id];
   const callback = (err, data) => {
-     res.send(data);
+    res.send(data);
   };
 
   db.query(query_string, payload, callback);
@@ -77,7 +79,7 @@ exports.get_log = (req, res, next) => {
   const query_string = "SELECT * from log where account_id = ?";  
   const payload = [req.params.account_id];
   const callback = (err, data) => {
-     res.send(data);
+    res.send(data);
   };
 
   db.query(query_string, payload, callback);
@@ -85,10 +87,11 @@ exports.get_log = (req, res, next) => {
 
 //View the bracket of games given a sport
 exports.get_bracket = (req, res, next) => {
-  const query_string = "SELECT * from sport, score where sport_type = ? order by field('elims', 'semi-finals', 'finals'), series;"; 
+  const query_string = "SELECT * from sport, score where sport_type = ? order "
+      +"by field('elims', 'semi-finals', 'finals'), series;"; 
   const payload = [req.params.sport_type];
   const callback = (err, data) => {
-     res.send(data);
+    res.send(data);
   };
 
   db.query(query_string, payload, callback);
@@ -99,7 +102,7 @@ exports.get_court = (req, res, next) => {
   const query_string = "SELECT * from court where court_id = ?";  
   const payload = [req.params.court_id];
   const callback = (err, data) => {
-     res.send(data);
+    res.send(data);
   };
 
   db.query(query_string, payload, callback);
@@ -110,7 +113,7 @@ exports.get_score = (req, res, next) => {
   const query_string = "SELECT * from score where score_id = ?";  
   const payload = [req.params.score_id];
   const callback = (err, data) => {
-     res.send(data);
+    res.send(data);
   };
 
   db.query(query_string, payload, callback);
@@ -121,7 +124,7 @@ exports.get_sponsor = (req, res, next) => {
   const query_string = "SELECT * from sponsor where sponsor_id = ?";  
   const payload = [req.params.sponsor_id];
   const callback = (err, data) => {
-     res.send(data);
+    res.send(data);
   };
 
   db.query(query_string, payload, callback);
@@ -132,7 +135,7 @@ exports.get_sport = (req, res, next) => {
   const query_string = "SELECT * from sport where sport_id = ?";  
   const payload = [req.params.sport_id];
   const callback = (err, data) => {
-     res.send(data);
+    res.send(data);
   };
 
   db.query(query_string, payload, callback);
@@ -143,7 +146,7 @@ exports.get_team = (req, res, next) => {
   const query_string = "SELECT * from team where team_id = ?";  
   const payload = [req.params.team_id];
   const callback = (err, data) => {
-     res.send(data);
+    res.send(data);
   };
 
   db.query(query_string, payload, callback);
