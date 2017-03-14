5dc2b94162f27e042a4ab1d2d346ed7f48f980d5
tidsoptimistharleyqueen <mariqueentenedero@gmail.com>
2017-03-14 03:57:48 -0700

    updated delete.js

--- backend/controller/delete.js
+++ backend/controller/delete.js
@@ -4,7 +4,7 @@ const db = require(__dirname + '/../lib/mariasql');
 
 //DELETE an ACCOUNT
 exports.delete_account = (req, res, next) => {
-  const query_string ='DELETE FROM account WHERE account_id = ?'; 
+  const query_string ='DELETE FROM account WHERE account_id = ?';
   const payload = [req.params.account_id];
   const callback = (err, data) => {
     res.send(data);
@@ -15,7 +15,7 @@ exports.delete_account = (req, res, next) => {
 
 //DELETE a PLAYER
 exports.delete_player = (req, res, next) => {
-  const query_string ='DELETE FROM player WHERE player_id = ?'; 
+  const query_string ='DELETE FROM player WHERE player_id = ?';
   const payload = [req.params.player_id];
   const callback = (err, data) => {
     res.send(data);
@@ -26,7 +26,7 @@ exports.delete_player = (req, res, next) => {
 
 //DELETE a GAME
 exports.delete_game = (req, res, next) => {
-  const query_string ='DELETE FROM game WHERE game_id = ?'; 
+  const query_string ='DELETE FROM game WHERE game_id = ?';
   const payload = [req.params.game_id];
   const callback = (err, data) => {
     res.send(data);
@@ -37,7 +37,7 @@ exports.delete_game = (req, res, next) => {
 
 //DELETE a SPORT
 exports.delete_sport = (req, res, next) => {
-  const query_string ='DELETE FROM sport WHERE sport_id = ?'; 
+  const query_string ='DELETE FROM sport WHERE sport_id = ?';
   const payload = [req.params.sport_id];
   const callback = (err, data) => {
     res.send(data);
@@ -48,7 +48,7 @@ exports.delete_sport = (req, res, next) => {
 
 //DELETE a SCORE
 exports.delete_score = (req, res, next) => {
-  const query_string ='DELETE FROM score WHERE score_id = ?'; 
+  const query_string ='DELETE FROM score WHERE score_id = ?';
   const payload = [req.params.score_id];
   const callback = (err, data) => {
     res.send(data);
@@ -59,7 +59,7 @@ exports.delete_score = (req, res, next) => {
 
 //DELETE a COURT
 exports.delete_court = (req, res, next) => {
-  const query_string ='DELETE FROM court WHERE court_id = ?'; 
+  const query_string ='DELETE FROM court WHERE court_id = ?';
   const payload = [req.params.court_id];
   const callback = (err, data) => {
     res.send(data);
@@ -70,7 +70,7 @@ exports.delete_court = (req, res, next) => {
 
 //DELETE a SPONSOR
 exports.delete_sponsor = (req, res, next) => {
-  const query_string ='DELETE FROM sponsor WHERE sponsor_id = ?'; 
+  const query_string ='DELETE FROM sponsor WHERE sponsor_id = ?';
   const payload = [req.params.sponsor_id];
   const callback = (err, data) => {
     res.send(data);
@@ -81,7 +81,7 @@ exports.delete_sponsor = (req, res, next) => {
 
 //DELETE a TEAM
 exports.delete_team = (req, res, next) => {
-  const query_string ='DELETE FROM team WHERE team_id = ?'; 
+  const query_string ='DELETE FROM team WHERE team_id = ?';
   const payload = [req.params.team_id];
   const callback = (err, data) => {
     res.send(data);
@@ -92,7 +92,7 @@ exports.delete_team = (req, res, next) => {
 
 //DELETE a LOG
 exports.delete_log = (req, res, next) => {
-  const query_string ='DELETE FROM log WHERE log_id = ?'; 
+  const query_string ='DELETE FROM log WHERE log_id = ?';
   const payload = [req.params.log_id];
   const callback = (err, data) => {
     res.send(data);
@@ -104,20 +104,8 @@ exports.delete_log = (req, res, next) => {
 //DELETE a PLAYER in a TEAM
 exports.delete_team_player = (req, res, next) => {
   const query_string ='DELETE FROM team_player WHERE player_id = ? and '
-      +'player_id = ?'; 
+      +'player_id = ?';
   const payload = [req.params.team_id,req.params.player_id];
-  const callback = (err, data) => {
-    res.send(data);
-  };
-
-  db.query(query_string, payload, callback);
-};
-
-//DELETE a SPORT of a PLAYER
-exports.delete_sport_player = (req, res, next) => {
-  const query_string ='DELETE FROM sport_player WHERE sport_id = ? and '
-      +'player_id = ?'; 
-  const payload = [req.params.sport_id,req.params.player_id];
   const callback = (err, data) => {
     res.send(data);
   };
@@ -128,7 +116,7 @@ exports.delete_sport_player = (req, res, next) => {
 //DELETE a REGISTER of a PLAYER
 exports.delete_game_player = (req, res, next) => {
   const query_string ='DELETE FROM registers WHERE game_id = ? and '
-      +'player_id = ?'; 
+      +'player_id = ?';
   const payload = [req.params.game_id,req.params.player_id];
   const callback = (err, data) => {
     res.send(data);
@@ -140,7 +128,7 @@ exports.delete_game_player = (req, res, next) => {
 //DELETE a SPONSOR in a GAME
 exports.delete_game_sponsor = (req, res, next) => {
   const query_string ='DELETE FROM game_sponsor WHERE game_id = ? and '
-      +'sponsor_id = ?'; 
+      +'sponsor_id = ?';
   const payload = [req.params.game_id,req.params.sponsor_id];
   const callback = (err, data) => {
    res.send(data);
@@ -152,7 +140,7 @@ exports.delete_game_sponsor = (req, res, next) => {
 //DELETE PARTICIPATES
 exports.delete_participates = (req, res, next) => {
   const query_string ='DELETE FROM participates WHERE player_id = ? and '
-      +'sport_id = ?'; 
+      +'sport_id = ?';
   const payload = [req.params.player_id,req.params.sport_id];
   const callback = (err, data) => {
   res.send(data);
@@ -164,7 +152,7 @@ exports.delete_participates = (req, res, next) => {
 //DELETE REGISTERS
 exports.delete_registers = (req, res, next) => {
   const query_string ='DELETE FROM registers WHERE player_id = ? and '
-      +'game_id = ?'; 
+      +'game_id = ?';
   const payload = [req.params.player_id,req.params.game_id];
   const callback = (err, data) => {
     res.send(data);
@@ -176,7 +164,7 @@ exports.delete_registers = (req, res, next) => {
 //DELETE GAME TYPE
 exports.delete_game_game_type = (req, res, next) => {
   const query_string ='DELETE FROM game_game_type WHERE game_id = ? and '
-      +'game_type = ?'; 
+      +'game_type = ?';
   const payload = [req.params.game_id,req.params.game_type];
   const callback = (err, data) => {
     res.send(data);
@@ -184,7 +172,3 @@ exports.delete_game_game_type = (req, res, next) => {
 
   db.query(query_string, payload, callback);
 };
-
-
-
-
