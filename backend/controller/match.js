b7ae965498533412d5f51603b151104b7a60078c
cyronquillo <cbronquillo@up.edu.ph>
2017-03-20 01:37:41 +0800

    merged attributes of score to match

--- backend/controller/match.js
+++ backend/controller/match.js
@@ -1,6 +1,11 @@
 exports.add_match_event = (req,res,next) => {
-  const query_string = 'INSERT into match_event(status,match_date_time,sport_id) VALUES (?,?,?)'; 
-  const payload = [req.body.status, req.body.match_date_time,req.body.sport_id];
+  const query_string = 'INSERT into match_event(status,' +
+  		'match_date_time, score1, score2, series,sport_id,' +
+  		' team1_id, team2_id, court_id) VALUES (?,?,?,?,?,?,?,?,?)'; 
+  const payload = [req.body.status, req.body.match_date_time,
+  		req.body.sport_id, req.body.score1, req.body.score2,
+  		req.body.series, req.body.sport_id, req.body.team1_id,
+  		req.body.team2_id, req.body.court_id];
   const callback = (err,data) => {
 	if(err){
 		res.status(500).send(err);
@@ -27,8 +32,15 @@ exports.get_match_event = (req, res, next) => {
 };
 
 exports.update_match_event = (req, res, next) => {
-	const query_string = 'UPDATE match_event set status = ?,match_date_time = ?, sport_id = ? WHERE match_id = ?';
-	const payload = [req.body.status,req.body.match_date_time,req.body.sport_id,req.body.match_id];
+	const query_string = 'UPDATE match_event set status = ?,' +
+		'match_date_time = ?, score1 = ?, score2 = ?, series = ?,' + 
+		' sport_id = ?, team1_id = ?, team2_id = ?, court_id = ? ' +
+		'WHERE match_id = ?';
+	const payload = [req.body.status, req.body.match_date_time,
+  		req.body.sport_id, req.body.score1, req.body.score2,
+  		req.body.series, req.body.sport_id, req.body.team1_id,
+  		req.body.team2_id, req.body.court_id, req.body.match_id];
+
     const callback = (err, data) => {
 		if(err){
 			res.status(500).send(err);
