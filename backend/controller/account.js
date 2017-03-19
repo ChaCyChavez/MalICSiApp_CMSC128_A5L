d1a3a6cb289b1bfff07afaf6e7ecd64389e593f9
cyronquillo <cbronquillo@up.edu.ph>
2017-03-20 02:25:58 +0800

    Added updated_account

--- backend/controller/account.js
+++ backend/controller/account.js
@@ -35,6 +35,25 @@ exports.add_account = (req,res,next) => {
   db.query(query_string, payload, callback);
 };
 
+exports.update_account = (req,res,next) => {
+    const query_string = 'UPDATE account set firstname = ?, middlename = ?,'+
+      'lastname = ?, username = ?, password = ?, course = ?, birthday = ?, '+
+      'college = ?, status = ?, is_game_head = ?, position = ?, is_player = ?'+
+      ',player_jersey_num = ?,player_role = ?, team_id = ? where account_id = ?'; 
+    const payload = [req.body.firstname, req.body.middlename, 
+        req.body.lastname, req.body.username, crypto.createHash('sha256')
+        .update(req.body.password).digest('base64'),req.body.course, 
+        req.body.birthday, req.body.college, req.body.status, 
+        req.body.is_game_head, req.body.position, req.body.is_player,
+        req.body.player_jersey_num, req.body.player_role, req.body.team_id,
+        req.body.account_id];
+  const callback = (err,data) => {
+    res.send(data);
+  };
+
+  db.query(query_string, payload, callback);
+};
+
 exports.get_account = (req, res, next) => {
   const query_string = "SELECT account_id, email, is_game_head,is_player "
       +"is_game_head, is_player,username, firstname, middlename, "
