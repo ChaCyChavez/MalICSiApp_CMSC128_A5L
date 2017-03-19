2d40538b2c5984358f5ed00ea32ce7000bb96efc
cyronquillo <cbronquillo@up.edu.ph>
2017-03-19 23:37:27 +0800

    Final database tables.sql

--- backend/database/tables.sql
+++ backend/database/tables.sql
@@ -1,466 +1,113 @@
--- MySQL dump 10.15  Distrib 10.0.29-MariaDB, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: localhost
--- ------------------------------------------------------
--- Server version	10.0.29-MariaDB-0ubuntu0.16.04.1
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `account`
---
-
-DROP TABLE IF EXISTS `account`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `account` (
-  `account_id` int(11) NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(256) NOT NULL,
-  `middlename` varchar(256) NOT NULL,
-  `lastname` varchar(256) NOT NULL,
-  `email` varchar(256) NOT NULL,
-  `username` varchar(256) NOT NULL,
-  `password` varchar(256) NOT NULL,
-  `course` varchar(256) NOT NULL,
-  `birthday` date NOT NULL,
-  `college` enum('CA','CAS','CDC','CEAT','CEM','CFNR','CHE','CPAf','CVM','SESAM','GS') NOT NULL,
-  `status` tinyint(1) DEFAULT NULL,
-  `is_game_head` tinyint(1) DEFAULT NULL,
-  `position` varchar(256) DEFAULT NULL,
-  `is_player` tinyint(1) DEFAULT NULL,
-  `player_jersey_num` int(11) DEFAULT NULL,
-  `team_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`account_id`),
-  KEY `fk_account_team` (`team_id`),
-  CONSTRAINT `fk_account_team` FOREIGN KEY (`team_id`) REFERENCES `team` (`team_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `account`
---
-
-LOCK TABLES `account` WRITE;
-/*!40000 ALTER TABLE `account` DISABLE KEYS */;
-/*!40000 ALTER TABLE `account` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `court`
---
-
-DROP TABLE IF EXISTS `court`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `court` (
-  `court_id` int(11) NOT NULL AUTO_INCREMENT,
-  `court_name` varchar(256) NOT NULL,
-  `court_location` varchar(256) NOT NULL,
-  `court_type` varchar(256) NOT NULL,
-  PRIMARY KEY (`court_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `court`
---
-
-LOCK TABLES `court` WRITE;
-/*!40000 ALTER TABLE `court` DISABLE KEYS */;
-/*!40000 ALTER TABLE `court` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `court_match_event`
---
-
-DROP TABLE IF EXISTS `court_match_event`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `court_match_event` (
-  `court_id` int(11) NOT NULL,
-  `match_id` int(11) NOT NULL,
-  PRIMARY KEY (`court_id`,`match_id`),
-  KEY `fk_court_match_match` (`match_id`),
-  CONSTRAINT `fk_court_match_court` FOREIGN KEY (`court_id`) REFERENCES `court` (`court_id`),
-  CONSTRAINT `fk_court_match_match` FOREIGN KEY (`match_id`) REFERENCES `match_event` (`match_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `court_match_event`
---
-
-LOCK TABLES `court_match_event` WRITE;
-/*!40000 ALTER TABLE `court_match_event` DISABLE KEYS */;
-/*!40000 ALTER TABLE `court_match_event` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `game_event`
---
-
-DROP TABLE IF EXISTS `game_event`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `game_event` (
-  `game_id` int(11) NOT NULL AUTO_INCREMENT,
-  `game_name` varchar(256) NOT NULL,
-  `game_starting_time_date` datetime NOT NULL,
-  `game_ending_time_date` datetime NOT NULL,
-  `account_id` int(11) NOT NULL,
-  PRIMARY KEY (`game_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `game_event`
---
-
-LOCK TABLES `game_event` WRITE;
-/*!40000 ALTER TABLE `game_event` DISABLE KEYS */;
-/*!40000 ALTER TABLE `game_event` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `game_event_sponsor`
---
-
-DROP TABLE IF EXISTS `game_event_sponsor`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `game_event_sponsor` (
-  `game_id` int(11) NOT NULL,
-  `sponsor_id` int(11) NOT NULL,
-  PRIMARY KEY (`game_id`,`sponsor_id`),
-  KEY `fk_game_sponsor_sponsor` (`sponsor_id`),
-  CONSTRAINT `fk_game_sponsor_game` FOREIGN KEY (`game_id`) REFERENCES `game_event` (`game_id`),
-  CONSTRAINT `fk_game_sponsor_sponsor` FOREIGN KEY (`sponsor_id`) REFERENCES `sponsor` (`sponsor_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `game_event_sponsor`
---
-
-LOCK TABLES `game_event_sponsor` WRITE;
-/*!40000 ALTER TABLE `game_event_sponsor` DISABLE KEYS */;
-/*!40000 ALTER TABLE `game_event_sponsor` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `log`
---
-
-DROP TABLE IF EXISTS `log`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `log` (
-  `log_id` int(11) NOT NULL AUTO_INCREMENT,
-  `log_description` varchar(256) NOT NULL,
-  `account_id` int(11) NOT NULL,
-  PRIMARY KEY (`log_id`),
-  KEY `fk_log_account` (`account_id`),
-  CONSTRAINT `fk_log_account` FOREIGN KEY (`account_id`) REFERENCES `account` (`account_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `log`
---
-
-LOCK TABLES `log` WRITE;
-/*!40000 ALTER TABLE `log` DISABLE KEYS */;
-/*!40000 ALTER TABLE `log` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `match_event`
---
-
-DROP TABLE IF EXISTS `match_event`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `match_event` (
-  `match_id` int(11) NOT NULL AUTO_INCREMENT,
-  `status` tinyint(1) DEFAULT NULL,
-  `match_date_time` datetime DEFAULT NULL,
-  `sport_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`match_id`),
-  KEY `fk_match_sport` (`sport_id`),
-  CONSTRAINT `fk_match_sport` FOREIGN KEY (`sport_id`) REFERENCES `sport` (`sport_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `match_event`
---
-
-LOCK TABLES `match_event` WRITE;
-/*!40000 ALTER TABLE `match_event` DISABLE KEYS */;
-/*!40000 ALTER TABLE `match_event` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `player`
---
-
-DROP TABLE IF EXISTS `player`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `player` (
-  `player_id` int(11) NOT NULL AUTO_INCREMENT,
-  `account_id` int(11) NOT NULL,
-  `player_jersey_number` int(11) NOT NULL,
-  `is_coach` tinyint(1) DEFAULT NULL,
-  `game_id` int(11) NOT NULL,
-  PRIMARY KEY (`player_id`),
-  KEY `fk_player_account` (`account_id`),
-  KEY `fk_player_game` (`game_id`),
-  CONSTRAINT `fk_player_account` FOREIGN KEY (`account_id`) REFERENCES `account` (`account_id`),
-  CONSTRAINT `fk_player_game` FOREIGN KEY (`game_id`) REFERENCES `game_event` (`game_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `player`
---
-
-LOCK TABLES `player` WRITE;
-/*!40000 ALTER TABLE `player` DISABLE KEYS */;
-/*!40000 ALTER TABLE `player` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `player_match_event`
---
-
-DROP TABLE IF EXISTS `player_match_event`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `player_match_event` (
-  `account_id` int(11) NOT NULL,
-  `match_id` int(11) NOT NULL,
-  PRIMARY KEY (`account_id`,`match_id`),
-  KEY `fk_player_match_match` (`match_id`),
-  CONSTRAINT `fk_player_match_account` FOREIGN KEY (`account_id`) REFERENCES `account` (`account_id`),
-  CONSTRAINT `fk_player_match_match` FOREIGN KEY (`match_id`) REFERENCES `match_event` (`match_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `player_match_event`
---
-
-LOCK TABLES `player_match_event` WRITE;
-/*!40000 ALTER TABLE `player_match_event` DISABLE KEYS */;
-/*!40000 ALTER TABLE `player_match_event` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `player_team`
---
-
-DROP TABLE IF EXISTS `player_team`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `player_team` (
-  `sport_id` int(11) NOT NULL,
-  `team_id` int(11) NOT NULL,
-  PRIMARY KEY (`sport_id`,`team_id`),
-  KEY `fk_player_team_team` (`team_id`),
-  CONSTRAINT `fk_player_team_sport` FOREIGN KEY (`sport_id`) REFERENCES `sport` (`sport_id`),
-  CONSTRAINT `fk_player_team_team` FOREIGN KEY (`team_id`) REFERENCES `team` (`team_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `player_team`
---
-
-LOCK TABLES `player_team` WRITE;
-/*!40000 ALTER TABLE `player_team` DISABLE KEYS */;
-/*!40000 ALTER TABLE `player_team` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `score`
---
-
-DROP TABLE IF EXISTS `score`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `score` (
-  `score_id` int(11) NOT NULL AUTO_INCREMENT,
-  `score_value` int(11) DEFAULT NULL,
-  `series` enum('elimination','semi-finals','finals') NOT NULL,
-  `match_id` int(11) DEFAULT NULL,
-  `team_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`score_id`),
-  KEY `fk_score_match_event` (`match_id`),
-  KEY `fk_score_team` (`team_id`),
-  CONSTRAINT `fk_score_match_event` FOREIGN KEY (`match_id`) REFERENCES `match_event` (`match_id`),
-  CONSTRAINT `fk_score_team` FOREIGN KEY (`team_id`) REFERENCES `team` (`team_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `score`
---
-
-LOCK TABLES `score` WRITE;
-/*!40000 ALTER TABLE `score` DISABLE KEYS */;
-/*!40000 ALTER TABLE `score` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `sponsor`
---
-
-DROP TABLE IF EXISTS `sponsor`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `sponsor` (
-  `sponsor_id` int(11) NOT NULL AUTO_INCREMENT,
-  `sponsor_name` varchar(256) NOT NULL,
-  `sponsor_logo` varchar(256) DEFAULT NULL,
-  `sponsor_affiliation` varchar(256) DEFAULT NULL,
-  PRIMARY KEY (`sponsor_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `sponsor`
---
-
-LOCK TABLES `sponsor` WRITE;
-/*!40000 ALTER TABLE `sponsor` DISABLE KEYS */;
-/*!40000 ALTER TABLE `sponsor` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `sport`
---
-
-DROP TABLE IF EXISTS `sport`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `sport` (
-  `sport_id` int(11) NOT NULL AUTO_INCREMENT,
-  `sport_type` varchar(256) NOT NULL,
-  `division` enum('men','women','mixed') NOT NULL,
-  `game_id` int(11) NOT NULL,
-  PRIMARY KEY (`sport_id`),
-  KEY `fk_sport_game` (`game_id`),
-  CONSTRAINT `fk_sport_game` FOREIGN KEY (`game_id`) REFERENCES `game_event` (`game_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `sport`
---
-
-LOCK TABLES `sport` WRITE;
-/*!40000 ALTER TABLE `sport` DISABLE KEYS */;
-/*!40000 ALTER TABLE `sport` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `sport_player`
---
-
-DROP TABLE IF EXISTS `sport_player`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `sport_player` (
-  `sport_id` int(11) NOT NULL,
-  `player_id` int(11) NOT NULL,
-  PRIMARY KEY (`sport_id`,`player_id`),
-  KEY `fk_sport_player_player` (`player_id`),
-  CONSTRAINT `fk_sport_player_player` FOREIGN KEY (`player_id`) REFERENCES `player` (`player_id`),
-  CONSTRAINT `fk_sport_player_sport` FOREIGN KEY (`sport_id`) REFERENCES `sport` (`sport_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `sport_player`
---
-
-LOCK TABLES `sport_player` WRITE;
-/*!40000 ALTER TABLE `sport_player` DISABLE KEYS */;
-/*!40000 ALTER TABLE `sport_player` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `team`
---
-
-DROP TABLE IF EXISTS `team`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `team` (
-  `team_id` int(11) NOT NULL AUTO_INCREMENT,
-  `team_name` varchar(256) NOT NULL,
-  `team_color` varchar(256) NOT NULL,
-  `team_coach` varchar(256) NOT NULL,
-  `game_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`team_id`),
-  KEY `fk_team_game` (`game_id`),
-  CONSTRAINT `fk_team_game` FOREIGN KEY (`game_id`) REFERENCES `game_event` (`game_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `team`
---
-
-LOCK TABLES `team` WRITE;
-/*!40000 ALTER TABLE `team` DISABLE KEYS */;
-/*!40000 ALTER TABLE `team` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `team_player`
---
-
-DROP TABLE IF EXISTS `team_player`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `team_player` (
-  `team_id` int(11) NOT NULL,
-  `player_id` int(11) NOT NULL,
-  PRIMARY KEY (`team_id`,`player_id`),
-  KEY `fk_team_player_player` (`player_id`),
-  CONSTRAINT `fk_team_player_player` FOREIGN KEY (`player_id`) REFERENCES `player` (`player_id`),
-  CONSTRAINT `fk_team_player_team` FOREIGN KEY (`team_id`) REFERENCES `team` (`team_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `team_player`
---
-
-LOCK TABLES `team_player` WRITE;
-/*!40000 ALTER TABLE `team_player` DISABLE KEYS */;
-/*!40000 ALTER TABLE `team_player` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2017-03-19 12:22:50
+DROP DATABASE IF EXISTS malicsi;
+CREATE DATABASE malicsi;
+USE malicsi;
+
+CREATE TABLE game_event (
+    game_id             int(11) NOT NULL AUTO_INCREMENT,
+    game_name           varchar(256) NOT NULL,
+    game_starting_time_date     datetime NOT NULL,
+    game_ending_time_date       datetime NOT NULL,
+    PRIMARY KEY         (game_id)
+);
+
+CREATE TABLE team (
+    team_id             int(11) NOT NULL AUTO_INCREMENT,
+    team_name           varchar(256) NOT NULL,
+    team_color          varchar(256) NOT NULL,
+    team_coach          varchar(256) NOT NULL,
+    game_id             int(11),
+    PRIMARY KEY         (team_id),
+    CONSTRAINT          `fk_team_game`
+        FOREIGN KEY (game_id) REFERENCES game_event (game_id)
+);
+
+CREATE TABLE account (
+    account_id          int(11) NOT NULL AUTO_INCREMENT,
+    firstname           varchar(256) NOT NULL,
+    middlename          varchar(256) NOT NULL,
+    lastname            varchar(256) NOT NULL,
+    email               varchar(256) NOT NULL,
+    username            varchar(256) NOT NULL,
+    password            varchar(256) NOT NULL,
+    course              varchar(256) NOT NULL,
+    birthday            date NOT NULL,
+    college             enum('CA','CAS','CDC','CEAT','CEM','CFNR','CHE','CPAf','CVM','SESAM','GS') NOT NULL,
+    status              boolean,
+    is_game_head        boolean,
+    position            varchar(256),
+    is_player           boolean,
+    player_jersey_num   int(11),
+    player_role         varchar(256),
+    team_id             int(11),
+    PRIMARY KEY                     (account_id),
+    CONSTRAINT         `fk_account_team`
+        FOREIGN KEY (team_id) REFERENCES team (team_id)
+);
+
+CREATE TABLE log (
+    log_id              int(11) NOT NULL AUTO_INCREMENT,
+    log_description     varchar(256) NOT NULL,
+    account_id          int(11) NOT NULL,
+    PRIMARY KEY         (log_id),
+    CONSTRAINT          `fk_log_account`
+        FOREIGN KEY (account_id) REFERENCES account (account_id)
+);
+
+CREATE TABLE court (
+    court_id            int(11) NOT NULL AUTO_INCREMENT,
+    court_name          varchar(256) NOT NULL,
+    court_location      varchar(256) NOT NULL,
+    court_type          varchar(256) NOT NULL,
+    PRIMARY KEY         (court_id)
+);
+
+CREATE TABLE sponsor (
+    sponsor_id          int(11) NOT NULL AUTO_INCREMENT,
+    sponsor_name        varchar(256) NOT NULL,
+    sponsor_logo        varchar(256),
+    sponsor_affiliation varchar(256),
+    PRIMARY KEY         (sponsor_id)
+);
+
+CREATE TABLE sport (
+    sport_id            int(11) NOT NULL AUTO_INCREMENT,
+    sport_type          varchar(256) NOT NULL,
+    division            enum('men','women','mixed') NOT NULL,
+    game_id             int(11) NOT NULL,
+    PRIMARY KEY         (sport_id),
+    CONSTRAINT          `fk_sport_game`
+        FOREIGN KEY (game_id) REFERENCES game_event (game_id)
+);
+
+CREATE TABLE match_event (
+    match_id            int(11) NOT NULL AUTO_INCREMENT,
+    status              boolean,
+    match_date_time     datetime,
+    sport_id            int(11),
+    PRIMARY KEY         (match_id),
+    CONSTRAINT          `fk_match_sport`
+        FOREIGN KEY (sport_id) REFERENCES sport (sport_id)
+);
+
+CREATE TABLE score (
+    score_id            int(11) NOT NULL AUTO_INCREMENT,
+    score_value         int(11),
+    series              enum('elimination','semi-finals','finals') NOT NULL,
+    match_id            int(11),
+    team_id             int(11),
+    PRIMARY KEY         (score_id),
+    CONSTRAINT          `fk_score_match_event` 
+        FOREIGN KEY (match_id) REFERENCES match_event (match_id),
+    CONSTRAINT          `fk_score_team` 
+        FOREIGN KEY (team_id) REFERENCES team (team_id)
+);
+
+CREATE TABLE game_event_sponsor (
+    game_id             int(11) NOT NULL,
+    sponsor_id          int(11) NOT NULL,
+    PRIMARY KEY         (game_id,sponsor_id),
+    CONSTRAINT          `fk_game_sponsor_game` 
+        FOREIGN KEY (game_id) REFERENCES game_event (game_id),
+    CONSTRAINT          `fk_game_sponsor_sponsor`
+        FOREIGN KEY (sponsor_id) REFERENCES sponsor (sponsor_id)
+);
