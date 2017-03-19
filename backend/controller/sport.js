ce3b4d81ebba72bcea286e9695ddca3740ce27d2
cyronquillo <cbronquillo@up.edu.ph>
2017-03-20 01:51:21 +0800

    modified to follow convention

--- backend/controller/sport.js
+++ backend/controller/sport.js
@@ -3,15 +3,16 @@
 const db = require(__dirname + '/../lib/mariasql');
 
 exports.add_sport = (req,res,next) => {
-  const query_string = 'INSERT into sport(sport_type,division,game_id) VALUES (?,?,?)'; 
-  const payload = [req.body.sport_type, req.body.division,req.body.game_id];
-  const callback = (err,data) => {
-	if(err){
-		res.status(500).send(err);
-	}else{
-		res.status(200).send(data);
-	}
-  };
+  	const query_string = 'INSERT into sport(sport_type,'+
+  			'division,game_id) VALUES (?,?,?)'; 
+  	const payload = [req.body.sport_type, req.body.division,req.body.game_id];
+  	const callback = (err,data) => {
+		if(err){
+			res.status(500).send(err);
+		}else{
+			res.status(200).send(data);
+		}
+  	};
 
   db.query(query_string, payload, callback);
 };
