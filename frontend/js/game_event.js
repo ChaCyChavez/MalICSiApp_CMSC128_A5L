995887290a550dcd0ee3373bfe49f0fe8284735b
Honey Grace Garay <hggaray@up.edu.ph>
2017-03-20 13:54:09 +0800

    game event js (add modal, etc)

--- frontend/js/game_event.js
+++ frontend/js/game_event.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
   $('.carousel.carousel-slider').carousel({fullWidth: true});
   $('.modal').modal({
-    // size: 'small',
     dismissible: true,
     opacity: 0.5,
     in_duration: 300,
@@ -10,10 +9,25 @@ $(document).ready(function(){
     ending_top: '10%',
   });
 
+  // $('.modal').modal({
+  //   dismissible: true,
+  //   opacity: 0.5,
+  //   in_duration: 300,
+  //   out_duration: 200,
+  //   starting_top: '4%',
+  //   ending_top: '10%',
+  // });
+
+
   $("#modal1").width($(".modal").width());
   $("#modal1").height($(".modal").height());
 
   $("#modal2").width($(".modal").width());
   $("#modal2").height($(".modal").height());
+
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
 
 });
