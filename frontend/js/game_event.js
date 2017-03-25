6b548451fff9521e6ed512278fe263577a86e4bd
Honey Grace Garay <hggaray@up.edu.ph>
2017-03-25 14:57:18 +0800

    Add files via upload

--- frontend/js/game_event.js
+++ frontend/js/game_event.js
@@ -9,15 +9,6 @@ $(document).ready(function(){
     ending_top: '10%',
   });
 
-  // $('.modal').modal({
-  //   dismissible: true,
-  //   opacity: 0.5,
-  //   in_duration: 300,
-  //   out_duration: 200,
-  //   starting_top: '4%',
-  //   ending_top: '10%',
-  // });
-
 
   $("#modal1").width($(".modal").width());
   $("#modal1").height($(".modal").height());
@@ -29,5 +20,9 @@ $(document).ready(function(){
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15 // Creates a dropdown of 15 years to control year
   });
+
+  var offset = 300,
+  offset_opacity = 1200,
+  scroll_top_duration = 700;
 
 });
