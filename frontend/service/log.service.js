3602c3da2f665922b736156f6a06a7bc5a7aac10
cyronquillo <cbronquillo@up.edu.ph>
2017-03-31 05:20:04 +0800

    added log service;

--- frontend/service/log.service.js
+++ frontend/service/log.service.js
@@ -13,7 +13,88 @@
 
 		function LogService($window, $http, $q) {
 
+			const add_log = function (data) {
+				let deferred = $q.defer();
+
+				$http({
+					method: 'POST',
+					params: data,
+					xhrFields: {withCredentials: true},
+					url: '/api/add-log',
+					headers: headers
+				})
+				.then(function(res) {
+					deferred.resolve(res.data);
+				}, function(err) {
+					deferred.reject(err.data);
+				})
+
+				return deferred.promise;
+			}
+
+
+			const update_log = function (data) {
+				let deferred = $q.defer();
+
+				$http({
+					method: 'POST',
+					params: data,
+					xhrFields: {withCredentials: true},
+					url: '/api/update-log',
+					headers: headers
+				})
+				.then(function(res) {
+					deferred.resolve(res.data);
+				}, function(err) {
+					deferred.reject(err.data);
+				})
+
+				return deferred.promise;
+			}
+
+			const get_log = function (data) {
+				let deferred = $q.defer();
+
+				$http({
+					method: 'GET',
+					params: data,
+					xhrFields: {withCredentials: true},
+					url: '/api/get-log/' + req.data.log_id,
+					headers: headers
+				})
+				.then(function(res) {
+					deferred.resolve(res.data);
+				}, function(err) {
+					deferred.reject(err.data);
+				})
+
+				return deferred.promise;
+			}
+
+			const delete_log = function (data) {
+				let deferred = $q.defer();
+
+				$http({
+					method: 'POST',
+					params: data,
+					xhrFields: {withCredentials: true},
+					url: '/api/delete-log/' + req.data.id,
+					headers: headers
+				})
+				.then(function(res) {
+					deferred.resolve(res.data);
+				}, function(err) {
+					deferred.reject(err.data);
+				})
+
+				return deferred.promise;
+			}
+
 			let service = {};
+			service.add_log 				= add_log;
+			service.update_log 				= update_log;
+			service.get_log 				= get_log;
+			service.delete_log				= delete_log;
 			return service;
 
 		}
