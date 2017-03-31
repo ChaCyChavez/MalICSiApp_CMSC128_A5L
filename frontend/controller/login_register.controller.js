aa2fc187b8e907e256a632cea85b064b50059620
cyronquillo <cbronquillo@up.edu.ph>
2017-03-31 22:28:52 +0800

    added login_register controllers & services

--- frontend/controller/login_register.controller.js
+++ frontend/controller/login_register.controller.js
@@ -1,15 +1,65 @@
+
 'use strict';
 
 (() => {
-    angular.module('app')
+    angular
+    	.module('app')
         .controller('login-register-controller', login_register_controller);
 
-    function login_register_controller($scope, $location) {
-        
+    function login_register_controller($scope, $location, LoginRegisterService) {
+         
+        $scope.info = {
+            username : undefined,
+            password : undefined
+        }
+
+        $scope.data = {
+            
+        }
+
         $scope.login = () => {
-        		$("#modal-login").modal('close');
-        		
-            $location.path("/game-event").replace();
+            if ($scope.info.username === undefined ||
+                $scope.info.username === '' ||
+                $scope.info.password === undefined ||
+                $scope.info.password === '') {
+                Materialize.toast("Please fill-out all the fields", 4000, 'teal');
+                $scope.info.username = '';
+                $scope.info.password = '';
+                $scope.info.username = undefined;
+                $scope.info.password = undefined;
+            } else {
+        		LoginRegisterService
+                    .retrieve_account($scope.info)
+                    .then(function(res) {
+                        $("#modal-login").modal('close');	
+                    }, function(err) {
+                        Materialize.toast(err.message, 4000, 'teal');
+                        $scope.info.username = '';
+                        $scope.info.password = '';
+                        $scope.info.username = undefined;
+                        $scope.info.password = undefined;
+                    })
+            }
+        }
+
+        $scope.register = () => {
+            if ($scope.info.username === undefined ||
+                $scope.info.username === '' ||
+                $scope.info.password === undefined ||
+                $scope.info.password === '') {
+                Materialize.toast("Please fill-out all the fields", 4000, 'teal');
+                $scope.info.username = '';
+                $scope.info.password = '';
+            } else {
+                LoginRegisterService
+                    .retrieve_account($scope.info)
+                    .then(function(res) {
+                        $("#modal-login").modal('close');   
+                    }, function(err) {
+                        Materialize.toast(err.message, 4000, 'teal');
+                    })
+            }
         }
     }
+
 })();
