4882851782306a3e331782cc6176dfa7766703cc
charlesjom <cumarquez@up.edu.ph>
2017-04-02 18:29:23 +0800

    moved add match button and add match modal to sport.html

--- frontend/controller/match.controller.js
+++ frontend/controller/match.controller.js
@@ -5,7 +5,7 @@
         .module('app')
         .controller('match-controller', match_controller);
 
-    function match_controller($scope, $location) {
+    function match_controller($scope, $location, MatchService) {
 
         $scope.view_profile = () => {
             window.location.href="#!/profile";
@@ -20,6 +20,29 @@
         
         $scope.back_to_home = () => {
             window.location.href="#!/game-event";
+        }
+
+        var x = $location.path().toString().split("/");
+        $scope.match_id = parseInt(x[x.length-1]);
+        $scope.match = {
+            match_id: 0,
+            status: "",
+            match_date_time: null,
+            series: "",
+            court_name: "",
+            court_location: "",
+            teams: []
+        }
+
+        $scope.init_match = () => {
+            MatchService
+                .init_match($scope.match_id)
+                .then(function(res) {
+                    console.log(res[0]);
+                    $scope.match = res[0][0];
+                }, function(err) {
+                    console.log(res[0]);
+                })
         }
     }
 })();
