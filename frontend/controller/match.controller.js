c5b0d4d8ede7ab0b713616a4a0d9eb0a3be7ee18
charlesjom <cumarquez@up.edu.ph>
2017-04-03 22:23:38 +0800

    integrate match page

--- frontend/controller/match.controller.js
+++ frontend/controller/match.controller.js
@@ -24,22 +24,39 @@
 
         let x = $location.path().toString().split("/");
         $scope.match_id = parseInt(x[x.length-1]);
-        $scope.match = {
-            match_id: 0,
-            status: "",
-            match_date_time: null,
-            series: "",
-            court_name: "",
-            court_location: "",
-            teams: []
-        }
+        $scope.court = undefined;
+        $scope.match = undefined;
+        $scope.match_teams_scores = [];
 
         $scope.init_match = () => {
             MatchService
                 .init_match($scope.match_id)
                 .then(function(res) {
-                    console.log(res[0]);
                     $scope.match = res[0][0];
+                    console.log(res[0]);
+                }, function(err) {
+                    console.log(res[0]);
+                })
+        }
+
+        $scope.init_match_court = () => {
+            MatchService
+                .init_match_court($scope.match_id)
+                .then(function(res) {
+                    $scope.court = res[0][0];
+                    console.log(res[0]);
+                }, function(err) {
+                    console.log(res[0]);
+                })
+        }
+
+        $scope.init_match_teams = () => {
+            MatchService
+                .init_match_teams_scores($scope.match_id)
+                .then(function(res) {
+                    $scope.match_teams_scores = res[0];
+                    console.log(res[0]);
+                    MatchService.determine_winner($scope.match_teams_scores);
                 }, function(err) {
                     console.log(res[0]);
                 })
--- frontend/services/match.service.js
+++ frontend/services/match.service.js
@@ -60,14 +60,58 @@
 				}, function(err) {
 					deferred.reject(err.data);
 				})
+				return deferred.promise;
+			}
+
+			const init_match_court = (data) => {
+				let deferred = $q.defer();
+				$http({
+					method: 'GET',
+					url: '/api/get-court-of-match/' + data,
+					headers: headers
+				})
+				.then(function(res) {
+					deferred.resolve(res.data);
+				}, function(err) {
+					deferred.reject(err.data);
+				})
+				return deferred.promise;
+			}
+
+			const init_match_teams_scores = (data) => {
+				let deferred = $q.defer();
+				$http({
+					method: 'GET',
+					url: '/api/get-teams-N-scores-of-match/' + data,
+					headers: headers
+				})
+				.then(function(res) {
+					deferred.resolve(res.data);
+				}, function(err) {
+					deferred.reject(err.data);
+				})
 
 				return deferred.promise;
+			}
+
+			const determine_winner = (data) => {
+				let indexOfWinner = 0;
+				let winnerScore = data[0].score;
+				for (let i = 0; i<data.length; i++) {
+					data[i].is_winner = false;
+					if (data[i].score > winnerScore) indexOfWinner = i;
+				}
+				data[indexOfWinner].is_winner = true;
 			}
 
 			let service = {};
 			service.add_match = add_match;
 			service.delete_match = delete_match;
-			service.init_matches = init_matches;
+			service.init_match = init_match;
+			service.init_match_court = init_match_court;
+			service.init_match_teams_scores = init_match_teams_scores;
+			service.determine_winner = determine_winner;
+			
 			return service;
 		}
 })();
