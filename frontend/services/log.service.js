c2bf48d4ec7b69592c73e029a96bafc5db3cdf67
cyronquillo <cbronquillo@up.edu.ph>
2017-04-03 21:19:41 +0800

    added log views

---
+++ frontend/services/log.service.js
@@ -0,0 +1,121 @@
+'use strict';
+
+(() => {
+	angular
+		.module('app')
+		.factory('LogService', LogService);
+
+		LogService.$inject = ['$window', '$http', '$q'];
+
+		const headers = {
+			'content-type': 'application/x-www-form-urlencoded'
+		};
+
+		function LogService($window, $http, $q) {
+
+			const add_log = function (data) {
+				let deferred = $q.defer();
+
+				$http({
+					method: 'POST',
+					params: data,
+					xhrFields: {withCredentials: true},
+					url: '/api/add-log',
+					headers: headers
+				})
+				.then(function(res) {
+					deferred.resolve(res.data);
+				}, function(err) {
+					deferred.reject(err.data);
+				})
+
+				return deferred.promise;
+			}
+
+
+			const update_log = function (data) {
+				let deferred = $q.defer();
+
+				$http({
+					method: 'POST',
+					params: data,
+					xhrFields: {withCredentials: true},
+					url: '/api/update-log',
+					headers: headers
+				})
+				.then(function(res) {
+					deferred.resolve(res.data);
+				}, function(err) {
+					deferred.reject(err.data);
+				})
+
+				return deferred.promise;
+			}
+ 
+			const get_log = function (data) {
+				let deferred = $q.defer();
+
+				$http({
+					method: 'GET',
+					params: data,
+					xhrFields: {withCredentials: true},
+					url: '/api/get-log/',
+					headers: headers
+				})
+				.then(function(res) {
+					deferred.resolve(res.data);
+				}, function(err) {
+					deferred.reject(err.data);
+				})
+
+				return deferred.promise;
+			}
+
+			const get_all_logs = function (data) {
+				let deferred = $q.defer();
+
+				$http({
+					method: 'GET',
+					params: data,
+					xhrFields: {withCredentials: true},
+					url: '/api/get-all-logs/',
+					headers: headers
+				})
+				.then(function(res) {
+					deferred.resolve(res.data);
+				}, function(err) {
+					deferred.reject(err.data);
+				})
+
+				return deferred.promise;
+			}
+
+			const delete_log = function (data) {
+				let deferred = $q.defer();
+
+				$http({
+					method: 'POST',
+					params: data,
+					xhrFields: {withCredentials: true},
+					url: '/api/delete-log/' + req.data.id,
+					headers: headers
+				})
+				.then(function(res) {
+					deferred.resolve(res.data);
+				}, function(err) {
+					deferred.reject(err.data);
+				})
+
+				return deferred.promise;
+			}
+
+			let service = {};
+			service.add_log 				= add_log;
+			service.update_log 				= update_log;
+			service.get_log 				= get_log;
+			service.delete_log				= delete_log;
+			service.get_all_logs			= get_all_logs;
+			return service;
+
+		}
+})();
