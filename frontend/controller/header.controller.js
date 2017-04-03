8875f75cd1851d859c60f708e44610598b672525
EddieVallejos <eddievallejos08@gmail.com>
2017-04-03 08:06:49 +0800

    Conventions in controller-frontend fixed

--- frontend/controller/header.controller.js
+++ frontend/controller/header.controller.js
@@ -8,35 +8,36 @@
     header_controller.$inject = ['$scope', '$location', '$routeParams', '$rootScope', 'ProfileService'];
 
     function header_controller($scope, $location, $routeParams, $rootScope, ProfileService) {
-    /*
-        $scope.info = 1
-        $scope.profile = {}
-        $scope.user_upcoming_events = {}
-        $scope.user_past_events = {}
-    */
-        ProfileService
-        .get_profile()
-            .then((data) => {
-            if (data[0].length != 0) {
-                $scope.profile = data[0][0];
-            }
-        });
+    
+        $scope.get_profile = () => {
+            ProfileService
+            .get_profile()
+                .then((data) => {
+                if (data[0].length != 0) {
+                    $scope.profile = data[0][0];
+                }
+            });
+        }
 
-        ProfileService
-            .get_user_upcoming_events()
-            .then((data) => {
-            if (data[0].length != 0) {
-                $scope.user_upcoming_events = data[0];
-            }
-        });
+        $scope.get_user_upcoming_events = () => {
+            ProfileService
+                .get_user_upcoming_events()
+                .then((data) => {
+                if (data[0].length != 0) {
+                    $scope.user_upcoming_events = data[0];
+                }
+            });
+        }
 
-        ProfileService
-            .get_user_past_events()
-            .then((data) => {
-            if (data[0].length != 0) {
-                $scope.user_past_events = data[0];
-            }
-        });
+        $scope.get_user_past_events = () => {
+            ProfileService
+                .get_user_past_events()
+                .then((data) => {
+                if (data[0].length != 0) {
+                    $scope.user_past_events = data[0];
+                }
+            });
+        }
 
         $scope.view_profile = () => {
             window.location.href="#!/profile/";
