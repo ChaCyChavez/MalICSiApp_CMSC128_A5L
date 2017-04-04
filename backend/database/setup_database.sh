70d71c60004dca6056db51ea62d037b46c084bd7
CarlJanoras <janorascarlxd@gmail.com>
2017-04-05 03:48:13 +0800

    readablity. removed unnecessary line(s)

--- backend/database/setup_database.sh
+++ backend/database/setup_database.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-echo "-------------Enter mysql ROOT password"
-read -p "enter root password: " user
-mysql -uroot -p"$user" < tables.sql     							&& echo "Created Tables"				&&
+read -p "Enter mysql root password: " user
+mysql -uroot -p"$user" < tables.sql     			&& echo "Created Tables"				&&
 mysql -uroot -p"$user" < create_procedures.sql		&& echo "Inserted Create Procedures"	&&
-mysql -uroot -p"$user" < retrieve_procedures.sql		&& echo "Inserted Retrieve Procedures"	&&
+mysql -uroot -p"$user" < retrieve_procedures.sql	&& echo "Inserted Retrieve Procedures"	&&
 mysql -uroot -p"$user" < update_procedures.sql		&& echo "Inserted Update Procedures"	&&
 mysql -uroot -p"$user" < delete_procedures.sql		&& echo "Inserted Delete Procedures"	&&
-mysql -uroot -p"$user" < triggers.sql					&& echo "Inserted Triggers"				&&
+mysql -uroot -p"$user" < triggers.sql				&& echo "Inserted Triggers"				&&
 mysql -uroot -p"$user" < dummy_data.sql				&& echo "Inserted Dummy Data"
