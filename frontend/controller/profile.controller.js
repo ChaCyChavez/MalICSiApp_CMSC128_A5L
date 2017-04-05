7241bf0b8e3c9d7bd80e755cdcc2053e8f319878
CarlJanoras <janorascarlxd@gmail.com>
2017-04-06 04:55:36 +0800

    uses ng-init

--- frontend/controller/profile.controller.js
+++ frontend/controller/profile.controller.js
@@ -8,35 +8,30 @@
     profile_controller.$inject = ['$scope', '$location', '$routeParams', 'ProfileService', 'LoginRegisterService'];
 
     function profile_controller($scope, $location, $routeParams, ProfileService, LoginRegisterService) {
-    /*
-        $scope.info = 1
-        $scope.profile = {}
-        $scope.user_upcoming_events = {}
-        $scope.user_past_events = {}
-    */
-        
-        ProfileService
-        .get_profile()
-            .then((data) => {
-            if (data[0].length != 0) {
-                $scope.profile = data[0][0];
-            }
-        });
+        $scope.get_profile_info = () => {
+            ProfileService
+            .get_profile()
+                .then((data) => {
+                if (data[0].length != 0) {
+                    $scope.profile = data[0][0];
+                }
+            });
 
-        ProfileService
-            .get_user_upcoming_events()
-            .then((data) => {
-            if (data[0].length != 0) {
-                $scope.user_upcoming_events = data[0];
-            }
-        });
+            ProfileService
+                .get_user_upcoming_events()
+                .then((data) => {
+                if (data[0].length != 0) {
+                    $scope.user_upcoming_events = data[0];
+                }
+            });
 
-        ProfileService
-            .get_user_past_events()
-            .then((data) => {
-            if (data[0].length != 0) {
-                $scope.user_past_events = data[0];
-            }
-        });
+            ProfileService
+                .get_user_past_events()
+                .then((data) => {
+                if (data[0].length != 0) {
+                    $scope.user_past_events = data[0];
+                }
+            });
+        }       
     }
 })();
