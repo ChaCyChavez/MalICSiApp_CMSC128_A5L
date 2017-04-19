617ce7cb4048ba98375e38e7dde57853b1908b4c
EddieVallejos <eddievallejos08@gmail.com>
2017-04-19 16:48:16 +0800

    view other user

--- frontend/controller/profile.controller.js
+++ frontend/controller/profile.controller.js
@@ -14,6 +14,8 @@
         $scope.user_upcoming_events = {}
         $scope.user_past_events = {}
     */
+
+
         $scope.get_profile_info = () => {
             ProfileService
             .get_profile()
@@ -38,6 +40,39 @@
                     $scope.user_past_events = data[0];
                 }
             });
+        }
+
+        $scope.get_profile_info2 = (account_id) => {
+            ProfileService
+            .get_profile2(account_id)
+                .then((data) => {
+                if (data[0].length != 0) {
+                    $scope.profile = data[0][0];
+                }
+            });
+
+            ProfileService
+                .get_user_upcoming_events2(account_id)
+                .then((data) => {
+                if (data[0].length != 0) {
+                    $scope.user_upcoming_events = data[0];
+                }
+            });
+
+            ProfileService
+                .get_user_past_events2(account_id)
+                .then((data) => {
+                if (data[0].length != 0) {
+                    $scope.user_past_events = data[0];
+                }
+            });
         }    
+        $scope.init_profile = () => {
+           if ($routeParams.account_id != undefined) {
+            $scope.get_profile_info2($routeParams.account_id);
+           } else {
+            $scope.get_profile_info();
+           }
+        }
     }
 })();
--- frontend/services/profile.service.js
+++ frontend/services/profile.service.js
@@ -12,6 +12,7 @@
 		};
 
 		function ProfileService($window, $http, $q, $httpParamSerializer) {
+
 			
 			const get_profile = () => {
 				let deferred = $q.defer();
@@ -20,6 +21,22 @@
 					method: 'GET',
 					xhrFields: {withCredentials: false},
 					url: '/api/get-account/',
+				})
+				.then(function(res) {
+					deferred.resolve(res.data);
+				}, function(err) {
+					deferred.reject(err.data);
+				})
+				return deferred.promise;
+			}
+
+			const get_profile2 = (data) => {
+				let deferred = $q.defer();
+
+				$http({
+					method: 'GET',
+					xhrFields: {withCredentials: false},
+					url: '/api/get-account/' + data,
 				})
 				.then(function(res) {
 					deferred.resolve(res.data);
@@ -62,10 +79,47 @@
 				return deferred.promise;
 			}
 
+
+			const get_user_upcoming_events2 = (data) => {
+				let deferred = $q.defer();
+
+				$http({
+					method: 'GET',
+					xhrFields: {withCredentials: false},
+					url: '/api/get-user-upcoming-events/' + data,
+					headers: headers
+				})
+				.then(function(res) {
+					deferred.resolve(res.data);
+				}, function(err) {
+					deferred.reject(err.data);
+				})
+				return deferred.promise;
+			}
+
+			const get_user_past_events2 = (data) => {
+				let deferred = $q.defer();
+
+				$http({
+					method: 'GET',
+					xhrFields: {withCredentials: false},
+					url: '/api/get-user-past-events/' + data,
+				})
+				.then(function(res) {
+					deferred.resolve(res.data);
+				}, function(err) {
+					deferred.reject(err.data);
+				})
+				return deferred.promise;
+			}
+
 			let service = {};
 			service.get_profile 				= get_profile;
 			service.get_user_past_events 		= get_user_past_events;
 			service.get_user_upcoming_events 	= get_user_upcoming_events;
+			service.get_profile2 				= get_profile2;
+			service.get_user_past_events2 		= get_user_past_events2;
+			service.get_user_upcoming_events2 	= get_user_upcoming_events2;
 			return service;
 		}
 })();
