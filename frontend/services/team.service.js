72ca45f5a5f77480a33c61968f64cd317ddf4e6c
CarlJanoras <janorascarlxd@gmail.com>
2017-04-19 15:12:36 +0800

    added get_team_name

--- frontend/services/team.service.js
+++ frontend/services/team.service.js
@@ -42,9 +42,24 @@
 				return deferred.promise;
 			}
 
+			const get_team_name = (data) => {
+				let deferred = $q.defer();
+				$http({
+					method: 'GET',
+					url: '/api/get-team-name/' + data,
+					headers: headers
+				})
+				.then(function(res) {
+					deferred.resolve(res.data);
+				}, function(err) {
+					deferred.reject(err.data);
+				});
+				return deferred.promise;
+			}
 			let service = {};
 			service.get_team_match = get_team_match;
 			service.get_team_profile = get_team_profile;
+			service.get_team_name = get_team_name;
 			return service;
 		}
 })();
