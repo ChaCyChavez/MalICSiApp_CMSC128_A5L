a9f9587ee76bbc019475d5633e09a2ea6dc251fd
CarlJanoras <janorascarlxd@gmail.com>
2017-04-19 16:34:22 +0800

    team matches

--- backend/database/retrieve_procedures.sql
+++ backend/database/retrieve_procedures.sql
@@ -141,35 +141,35 @@ DROP PROCEDURE IF EXISTS get_team_profile//
   END;
 //
 
+
 DROP PROCEDURE IF EXISTS get_team_match//
   CREATE PROCEDURE get_team_match(
 	  IN _team_id int
   )
   BEGIN
-    SELECT
-		match_date_time, 
-		team_name, 
-		score,
-		match_id
-    FROM
-		team
-	NATURAL JOIN
-		match_event_team
-	NATURAL JOIN
-		match_event
-    WHERE 
-    	match_id 
-    IN (
-		SELECT
-			match_id
-      	FROM
-			match_event_team
-		WHERE
-			team_id = _team_id
-		)
-	ORDER BY
-		match_id;
+    SELECT 
+    	t.team_name, 
+    	m.match_date_time, 
+    	m.series, 
+    	m.match_id
+    FROM 
+    	team t 
+    NATURAL JOIN 
+    	match_event_team me 
+    NATURAL JOIN 
+    	match_event m
+    WHERE match_id in (
+    	SELECT 
+    		m.match_id
+      	FROM 
+      		team t 
+      	NATURAL JOIN 
+      		match_event_team me 
+      	NATURAL JOIN 
+      		match_event m
+      WHERE m.match_id = me.match_id && t.team_id = _team_id) && t.team_id != _team_id;
   END;
+
 //
 
 DROP PROCEDURE IF EXISTS get_team_name//
