3b41d9c93713ecb98a1dd411f8851b2727d94549
cyronquillo <cbronquillo@up.edu.ph>
2017-05-03 08:43:00 +0800

    participants page redirection;

--- frontend/controller/participant.controller.js
+++ frontend/controller/participant.controller.js
@@ -35,6 +35,10 @@
     }
 
     $scope.load_participants = () => {
+      if($rootScope.profile === undefined){
+        window.location.href="#!/";
+        return;
+      }
       $scope.participants = [];
       var s = [];
       var profiles = [];
