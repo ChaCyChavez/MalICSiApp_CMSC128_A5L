6ee94cc1b052c2f5a59ced13ee5cd9e5687ef7c2
mightymercado <mightymercado@gmail.com>
2017-05-11 04:34:17 +0800

    try fix 2

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,8 @@ start = () => {
         secret: config.COOKIE_SECRET,
         resave: false,
         saveUninitialized: true,
-        cookie: {maxAge: 60 * 1000 * 60 * 2, path: '/'},
+        cookie: {maxAge: 60 * 1000 * 60 * 2, secure : true},
+        proxy : true,
         store: new redis_store({
             host: 'redis-12382.c9.us-east-1-4.ec2.cloud.redislabs.com',
             port: '12382',
